06174151d3c8687e2ce35179e04386ccde4882f3
Ashley Smith <ashleymarie4989@gmail.com>
2023-12-05 13:16:10 -0800

    return resp from server for deployment

--- src/app/api/all-shifts/route.ts
+++ src/app/api/all-shifts/route.ts
@@ -3,10 +3,9 @@ import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
   try {
-    console.log('hello!!!')
     // const shifts = await sql`SELECT * FROM Shifts;`;
-    // return NextResponse.json({ shifts }, { status: 200 });
+    return NextResponse.json({ shifts: [] }, { status: 200 });
   } catch (error) {
-    // return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
